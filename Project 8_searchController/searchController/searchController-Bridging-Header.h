45cdb2b39b1ca7d8542f14723b773b72efb5c430
yumin <yumin8312@gmail.com>
2016-10-04 13:27:56 +0800

    solve Bridging-Header.h

---
+++ Project 8_searchController/searchController/searchController-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  searchController-Bridging-Header.h
+//  searchController
+//
+//  Created by yumin on 2016/10/4.
+//  Copyright © 2016年 yumin. All rights reserved.
+//
+
+#ifndef searchController_Bridging_Header_h
+#define searchController_Bridging_Header_h
+
+
+#endif /* searchController_Bridging_Header_h */
